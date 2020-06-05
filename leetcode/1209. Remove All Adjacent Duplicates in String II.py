da9632c5a74cfff328a1610c5cac4030f748e6ac
Chintan Parikh <chintan8195@gmail.com>
2020-06-05 01:17:23 -0700

    1209 with 2 pointer approach

--- leetcode/1209. Remove All Adjacent Duplicates in String II.py
+++ leetcode/1209. Remove All Adjacent Duplicates in String II.py
@@ -20,7 +20,7 @@ Input: s = "pbbcggttciiippooaais", k = 2
 Output: "ps"
 
 '''
-
+# Stack solution
 def remove_adjacent(s,k):
     stack =[['#',0]]
     for c in s:
@@ -32,6 +32,28 @@ def remove_adjacent(s,k):
             stack.append([c,1])
         print (stack)
     return ''.join(c*k for c,k in stack)
-print(remove_adjacent("deeedbbcccbdaa",3))
-print(remove_adjacent(s = "pbbcggttciiippooaais", k = 2))
-print(remove_adjacent("abcd",2))
+# print(remove_adjacent("deeedbbcccbdaa",3))
+# print(remove_adjacent(s = "pbbcggttciiippooaais", k = 2))
+# print(remove_adjacent("abcd",2))
+
+# 2 pointer solution
+def remove_adjacent_pointers(S,k):
+    i,n=0,len(S)
+    count = [0]*n
+    res = list(S)
+    for j in range(n):
+        print(res,i,j,count)
+        res[i]=res[j]
+        if i>0 and res[i-1]==res[j]:
+            count[i] = count[i-1]+1
+        else:
+            count[i]=1
+        if count[i]==k: i-=k
+        i+=1
+    print(res)
+    return ''.join(res[:i])
+
+print(remove_adjacent_pointers("deeedbbcccbdaa",3))
+print(remove_adjacent_pointers(S = "pbbcggttciiippooaais", k = 2))
+print(remove_adjacent_pointers("abcd",2))
+
