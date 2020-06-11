be5784ea8dd51edae6bc3785fb91eaf49b61ac37
Chintan Parikh <chintan8195@gmail.com>
2020-06-10 22:54:17 -0700

    1169

--- leetcode/1169. Invalid Transactions.py
+++ leetcode/1169. Invalid Transactions.py
@@ -1,3 +1,19 @@
 '''
 Invalid Transaction
+
+
 '''
+
+def invalid_transactions(transactions):
+    invalid = []
+    for i,t1 in enumerate(transactions):
+        name1,time1,amount1,city1 = t1.split(",")
+        if int(amount1)>1000:
+            invalid.append(t1)
+            continue
+        for j,t2 in enumerate(transactions):
+            if i!=j:
+                name2,time2,amount2,city2 = transactions.split(",")
+                if name1==name2 and city1!=city2 and abs(int(time1)-int(time2))<=60:
+                    invalid.append(t2)
+    return invalid
