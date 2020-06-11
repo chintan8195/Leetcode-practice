1dd5b27f39ee80636bdcc329ed65c4c6d6f5545b
Chintan Parikh <chintan8195@gmail.com>
2020-06-10 22:09:02 -0700

    797

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Leetcode-practice
 
-### www.leetcode.com/coders-block
+### https://leetcode.com/coders-block/
 
 Started this repository recently inorder to keep track of the problems I have solved and easily revise them from time to time.
 
--- leetcode/797. All Paths From Source to Target.py
+++ leetcode/797. All Paths From Source to Target.py
@@ -9,3 +9,14 @@ v    v
 2--->3
 There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
 '''
+
+def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+    def dfs(curr,path):
+        if curr==len(graph)-1:
+            res.append(path)
+        else:
+            for i in graph[curr]:
+                dfs(i,path+[i]) 
+    res=[]
+    dfs(0,[0])
+    return res
